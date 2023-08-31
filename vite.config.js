5f7d8c2716f7068ab1ab3c700a3ea58c474b886a
Sk Alijan <alizaan8695@gmail.com>
2023-08-31 16:00:01 +0530

    setup gh-pages

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,6 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  define: {
-    __APP_ENV__: process.env.VITE_VERCEL_ENV,
-  },
 });
